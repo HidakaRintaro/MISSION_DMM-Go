0f912fe2eac8124436f2808c71aa6d26527bc6cd
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-09-05 23:10:10 +0900

    add statuses/router.go

--- app/handler/statuses/router.go
+++ app/handler/statuses/router.go
@@ -1,1 +1,22 @@
 package statuses
+
+import (
+	"net/http"
+	"yatter-backend-go/app/app"
+
+	"github.com/go-chi/chi"
+)
+
+type handler struct {
+	app *app.App
+}
+
+// Create Handler for `/v1/statuses`
+func NewRouter(app *app.App) http.Handler {
+	r := chi.NewRouter()
+
+	h := &handler{app: app}
+	r.Post("/", h.Create)
+
+	return r
+}
