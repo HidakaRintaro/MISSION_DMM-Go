792d5fcad74091ef98b61ced36a0a80123814c0c
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-09-05 18:05:35 +0900

    fin step2

--- app/domain/repository/account.go
+++ app/domain/repository/account.go
@@ -9,5 +9,8 @@ import (
 type Account interface {
 	// Fetch account which has specified username
 	FindByUsername(ctx context.Context, username string) (*object.Account, error)
-	// TODO: Add Other APIs
+	// Create an account
+	CreateAccount(ctx context.Context, username string, passwordHash string) (int64, error)
+	// Fetch account which has specified id
+	FindById(ctx context.Context, id int64) (*object.Account, error)
 }
